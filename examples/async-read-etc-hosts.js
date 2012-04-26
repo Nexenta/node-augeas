77806e14b46a2a8f1913b71501dd0793420d8962
Igor Pashev <pashev.igor@gmail.com>
2012-04-26 18:36:26 +0400

    Added simple example for async reading /etc/hosts

---
+++ examples/async-read-etc-hosts.js
@@ -0,0 +1,35 @@
+var aug = require('../build/Release/libaugeas');
+
+
+/* 
+ * Create Augeas object to work with '/etc/hosts'
+ * through 'hosts' lens:
+ */
+aug.createAugeas(
+        {lens: 'hosts', incl: '/etc/hosts'},
+        function(aug) {
+            if (!aug.error()) {
+                console.log(aug.get('/files/etc/hosts/1/ipaddr'));
+                console.log(aug.match('/files/etc/hosts/*/*'));
+            } else {
+                console.log('Sad, but true :-(');
+            }
+        }
+);
+console.log('Waiting for Augeas ...');
+
+/* Example output:
+Waiting for Augeas ...
+127.0.0.1
+[ '/files/etc/hosts/1/ipaddr',
+  '/files/etc/hosts/1/canonical',
+  '/files/etc/hosts/1/alias[1]',
+  '/files/etc/hosts/1/alias[2]',
+  '/files/etc/hosts/1/alias[3]',
+  '/files/etc/hosts/1/alias[4]',
+  '/files/etc/hosts/2/ipaddr',
+  '/files/etc/hosts/2/canonical',
+  '/files/etc/hosts/3/ipaddr',
+  '/files/etc/hosts/3/canonical' ]
+*/
+
